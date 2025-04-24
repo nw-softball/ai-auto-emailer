60b92ff6ce3d31b754cf1f8d97470a7358d0a7f5
JJ Asghar <awesome@ibm.com>
2025-04-24 15:54:33 -0500

    v1.0.0: Public release.
    First real version of this application.
    
    Huzzah to v1.0.0!
    
    Signed-off-by: JJ Asghar <awesome@ibm.com>

---
+++ README.md
@@ -0,0 +1,84 @@
+# ai-auto-emailer
+
+## Scope
+
+This is an example helper email to send conference sponsorship emails to prospective sponsors for an event. This can be edited for
+your event easily with some changes around the HTML email and the "template email."
+
+This is designed to help community driven events to ask for money from companies, this will **save hours of work** for community volunteers.
+
+## Configuration and setup
+
+### PreSteps
+
+0. Have [ollama][ollama] and python installed.
+
+1. Clone and set up the working space.
+```bash
+git clone https://github.com/jjasghar/ai-auto-emailer.git
+cd ai-auto-emailer
+python3.11 -m venv venv
+pip install -r requirements.txt
+```
+
+2. Copy the `config.toml.example` and edit the `config.toml`. NOTE: the `EMAIL_TEMPLATE` is the email that you
+want the LLM to take inspiration from.
+```bash
+cp config.toml.example config.toml
+vim config.toml
+```
+
+3. Edit the `names.csv`
+```bash
+vim names.csv # might I suggest just your name only to start out with.
+```
+
+4. Run a test run. NOTE: use `DEBUG=True` before `python main.py` to get DEBUG
+   output in the `log/main_logging.log`.
+```bash
+python main.py
+SMTP password: super_secret_p@ssw0rd!
+>>>> email sent to billy@example.com <<<<<
+>>>> email sent to mark@example.com <<<<<
+>>>> email sent to YOUREMAIL@gmail.com <<<<<
+This took 0:01:35.220344 to run
+```
+
+5. Verify in your inbox
+6. If you like it, change the `names.csv` to the people you want to start emailing.
+
+## Using Gmail as your SMTP server
+
+Set the following settings for `Gmail` to work.
+```
+SMTP_SERVER="smtp.gmail.com"
+SMTP_PORT=465
+SMTP_SENDER_EMAIL="YOUREMAIL@gmail.com"
+```
+The password for `SMTP_PASSWORD` will need to be your Gmail password, but if
+you have "[Application Password](https://myaccount.google.com/apppasswords)"
+you'll need to create a password for this application.
+
+## License & Authors
+
+If you would like to see the detailed LICENSE click [here](./LICENSE).
+
+- Author: JJ Asghar <awesome@ibm.com>
+
+```text
+Copyright:: 2025- IBM, Inc
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
+
+[ollama]: https://ollama.com
---
+++ main.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+import os.path
+if not os.path.exists("config.toml"):
+    print("*****")
+    print("You need to have a config.toml for this application to work, check config.toml.example for options")
+    print("*****")
+    exit(1)
+
+from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from jinja2 import Environment, FileSystemLoader
+from ollama import chat
+import csv
+import logging
+import markdown
+import os
+import smtplib, ssl
+import tomllib
+
+LOGGING_LEVEL=os.environ.get("DEBUG", "INFO")
+
+start_time = datetime.now()
+if LOGGING_LEVEL:
+    logging.basicConfig(filename="log/main_logging.log",
+                        level=logging.DEBUG,
+                        format="%(asctime)s %(levelname)s %(message)s",
+                        datefmt="%Y-%m-%d %H:%M:%S",)
+else:
+    logging.basicConfig(filename="log/main_logging.log",
+                        level=logging.INFO,
+                        format="%(asctime)s %(levelname)s %(message)s",
+                        datefmt="%Y-%m-%d %H:%M:%S",)
+
+with open("config.toml", "rb") as f:
+    config = tomllib.load(f)
+
+port = config["SMTP_PORT"]
+password = input("SMTP password: ")
+sender_email = config["SMTP_SENDER_EMAIL"]
+
+environment = Environment(loader=FileSystemLoader("templates/"))
+template = environment.get_template("email.html.j2")
+
+with open(config["CSV_FILE"]) as data_file:
+    data = csv.DictReader(data_file)
+    data_list = []
+    for row in data:
+        data_list.append(row)
+
+city_event = config["CITY_EVENT"]
+template_email = config["EMAIL_TEMPLATE"]
+prospectus = config["EMAIL_PROSPECTUS_LINK"]
+date_of_event = config["EMAIL_DATE_OF_EVENT"]
+logo_url = config["EMAIL_LOGO_URL"]
+sponsor_url = config["EMAIL_SPONSOR_URL"]
+
+your_name = config["YOUR_NAME"]
+your_phone = config["YOUR_PHONE"]
+your_email = config["YOUR_EMAIL"]
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(config["SMTP_SERVER"], port, context=context) as server:
+    server.login(sender_email, password)
+
+    for i in data_list:
+
+        receiver_email = i['email']
+
+        response = chat(model=config["OLLAMA_AI_MODEL"], messages= [
+        {
+            'role': 'user',
+            'content': f"""You are a professional cold caller for events. You
+            write friendly engaging emails to help drive people to want to
+            sponser your events. You use the following information and template
+            email to help write out unique and engaging conversational emails:
+            {template_email}. The date for the event is: {date_of_event}, and
+            if you want to link to the prospectus it is located here:
+            {prospectus}. Write an email that convinces me, the reader, and the
+            recipient of this email to want to sponsor this event. The
+            recipients name is {i['name']}. The email is from my name which is
+            {your_name}, and my email address and {your_email}, my phone number
+            is {your_phone}. Please do not put the Subject at the top of the
+            email, or mention any company I work for.""",
+        },]
+    )
+
+        message = MIMEMultipart("alternative")
+        message["Subject"] = config["EMAIL_SUBJECT"]
+        message["From"] = sender_email
+        message["To"] = receiver_email
+
+        raw_response =  response['message']['content']
+        text_email = raw_response
+
+        markdown_email = markdown.markdown(raw_response)
+        html_email = template.render(content=markdown_email,
+                                     sponsor_url=sponsor_url,
+                                     city_event=city_event,
+                                     logo_url=logo_url
+                                    )
+        part1 = MIMEText(text_email, "plain")
+        part2 = MIMEText(html_email, "html")
+
+        message.attach(part1)
+        message.attach(part2)
+        logging.debug(f"")
+        logging.debug(f"Sending email to {receiver_email}...")
+        logging.debug(f"")
+        logging.debug(f"Email: {message.as_string()}")
+
+        server.sendmail(sender_email, receiver_email, message.as_string())
+        logging.info(f">>>> email sent to {i['email']} <<<<<")
+        print(f">>>> email sent to {i['email']} <<<<<")
+
+print(f"This took {datetime.now() - start_time} to run")
+logging.info(f"This took {datetime.now() - start_time} to run")
