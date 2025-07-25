9830d67c66c0a6911ddb413c415c6bc26f25e1ef
JJ Asghar <awesome@ibm.com>
2025-07-25 13:42:59 -0500

    added a way to just send out the template email, instead of the ai

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from email.mime.text import MIMEText
 from jinja2 import Environment, FileSystemLoader
 from ollama import chat
 from tqdm import tqdm
+import argparse
 import csv
 import logging
 import markdown
@@ -20,100 +21,115 @@ import smtplib, ssl
 import tomllib
 import time
 
-LOGGING_LEVEL=os.environ.get("DEBUG", "INFO")
+def main():
 
-start_time = datetime.now()
-if LOGGING_LEVEL:
-    logging.basicConfig(filename="log/main_logging.log",
-                        level=logging.DEBUG,
-                        format="%(asctime)s %(levelname)s %(message)s",
-                        datefmt="%Y-%m-%d %H:%M:%S",)
-else:
-    logging.basicConfig(filename="log/main_logging.log",
-                        level=logging.INFO,
-                        format="%(asctime)s %(levelname)s %(message)s",
-                        datefmt="%Y-%m-%d %H:%M:%S",)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--ai', action='store_true')
 
-with open("config.toml", "rb") as f:
-    config = tomllib.load(f)
+    args = parser.parse_args()
 
-port = config["SMTP_PORT"]
-password = input("SMTP password: ")
-sender_email = config["SMTP_SENDER_EMAIL"]
+    LOGGING_LEVEL=os.environ.get("DEBUG", "INFO")
 
-environment = Environment(loader=FileSystemLoader("templates/"))
-template = environment.get_template("email.html.j2")
+    start_time = datetime.now()
+    if LOGGING_LEVEL:
+        logging.basicConfig(filename="log/main_logging.log",
+                            level=logging.DEBUG,
+                            format="%(asctime)s %(levelname)s %(message)s",
+                            datefmt="%Y-%m-%d %H:%M:%S",)
+    else:
+        logging.basicConfig(filename="log/main_logging.log",
+                            level=logging.INFO,
+                            format="%(asctime)s %(levelname)s %(message)s",
+                            datefmt="%Y-%m-%d %H:%M:%S",)
 
-with open(config["CSV_FILE"]) as data_file:
-    data = csv.DictReader(data_file)
-    data_list = []
-    for row in data:
-        data_list.append(row)
+    with open("config.toml", "rb") as f:
+        config = tomllib.load(f)
 
-event_website = config["EMAIL_EVENT_WEBSITE"]
-template_email = config["EMAIL_TEMPLATE"]
-logo_url = config["EMAIL_LOGO_URL"]
-sponsor_url = config["EMAIL_SPONSOR_URL"]
+    port = config["SMTP_PORT"]
+    password = input("SMTP password: ")
+    sender_email = config["SMTP_SENDER_EMAIL"]
 
-your_name = config["YOUR_NAME"]
-your_email = config["YOUR_EMAIL"]
-email_date = time.strftime("%Y-%m-%d")
+    environment = Environment(loader=FileSystemLoader("templates/"))
+    template = environment.get_template("email.html.j2")
 
-context = ssl.create_default_context()
-with smtplib.SMTP_SSL(config["SMTP_SERVER"], port, context=context) as server:
-    server.login(sender_email, password)
+    with open(config["CSV_FILE"]) as data_file:
+        data = csv.DictReader(data_file)
+        data_list = []
+        for row in data:
+            data_list.append(row)
 
-    for i in tqdm(data_list, desc="Sending emails to..."):
+    event_website = config["EMAIL_EVENT_WEBSITE"]
+    template_email = config["EMAIL_TEMPLATE"]
+    logo_url = config["EMAIL_LOGO_URL"]
+    sponsor_url = config["EMAIL_SPONSOR_URL"]
 
-        receiver_email = i['email']
+    your_name = config["YOUR_NAME"]
+    your_email = config["YOUR_EMAIL"]
+    email_date = time.strftime("%Y-%m-%d")
 
-        response = chat(model=config["OLLAMA_AI_MODEL"], messages= [
-        {
-            'role': 'user',
-            'content': f"""
-            You are a professional cold caller for sponsorships for 50c3. You
-            write friendly engaging emails to help drive people to want to
-            sponsor your events. You use the following information and template
-            email to help write out unique and engaging conversational emails:
-            {template_email}. The event website is located at this url:
-            {event_website}. Write an email that convinces me and the recipient
-            of this email to want to sponsor this organization. The recipients
-            name is {i['name']}. The email is from my name which is
-            {your_name}, and my email address and {your_email}. Please do not
-            put the Subject at the top of the email, or mention any company I
-            work for. Do not add my phone number or my title.
-            """,
-        },]
-    )
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(config["SMTP_SERVER"], port, context=context) as server:
+        server.login(sender_email, password)
 
-        message = MIMEMultipart("alternative")
-        message["Subject"] = config["EMAIL_SUBJECT"]
-        message["From"] = sender_email
-        message["To"] = receiver_email
-        message["Reply-To"] = f"fof@nwsoftball.org"
+        for i in tqdm(data_list, desc="Sending emails to..."):
 
-        raw_response =  response['message']['content']
-        text_email = raw_response
+            receiver_email = i['email']
 
-        markdown_email = markdown.markdown(raw_response)
-        html_email = template.render(content=markdown_email,
-                                     logo_url=logo_url,
-                                     sponsor_url=sponsor_url,
-                                     email_date=email_date
-                                    )
-        part1 = MIMEText(text_email, "plain")
-        part2 = MIMEText(html_email, "html")
+            if args.ai == True:
+                response = chat(model=config["OLLAMA_AI_MODEL"], messages= [
+                {
+                    'role': 'user',
+                    'content': f"""
+                    You are a professional cold caller for sponsorships for 50c3. You
+                    write friendly engaging emails to help drive people to want to
+                    sponsor your events. You use the following information and template
+                    email to help write out unique and engaging conversational emails:
+                    {template_email}. The event website is located at this url:
+                    {event_website}. Write an email that convinces me and the recipient
+                    of this email to want to sponsor this organization. The recipients
+                    name is {i['name']}. The email is from my name which is
+                    {your_name}, and my email address and {your_email}. Please do not
+                    put the Subject at the top of the email, or mention any company I
+                    work for. Do not add my phone number or my title.
+                    """,
+                },
+                ]
+        )
+                raw_response =  response['message']['content']
+            else:
+                raw_response = f"Dear {i['name']},\n\n"
+                raw_response += template_email
 
-        message.attach(part1)
-        message.attach(part2)
-        logging.debug(f"")
-        logging.debug(f"Sending email to {receiver_email}...")
-        logging.debug(f"")
-        logging.debug(f"Email: {message.as_string()}")
+            message = MIMEMultipart("alternative")
+            message["Subject"] = config["EMAIL_SUBJECT"]
+            message["From"] = sender_email
+            message["To"] = receiver_email
+            message["Reply-To"] = f"fof@nwsoftball.org"
 
-        server.sendmail(sender_email, receiver_email, message.as_string())
-        logging.info(f">>>> email sent to {i['email']} <<<<<")
-        print(f">>>> email sent to {i['email']} <<<<<")
+            text_email = raw_response
 
-print(f"This took {datetime.now() - start_time} to run")
-logging.info(f"This took {datetime.now() - start_time} to run")
+            markdown_email = markdown.markdown(raw_response)
+            html_email = template.render(content=markdown_email,
+                                         logo_url=logo_url,
+                                         sponsor_url=sponsor_url,
+                                         email_date=email_date
+                                        )
+            part1 = MIMEText(text_email, "plain")
+            part2 = MIMEText(html_email, "html")
+
+            message.attach(part1)
+            message.attach(part2)
+            logging.debug(f"")
+            logging.debug(f"Sending email to {receiver_email}...")
+            logging.debug(f"")
+            logging.debug(f"Email: {message.as_string()}")
+
+            server.sendmail(sender_email, receiver_email, message.as_string())
+            logging.info(f">>>> email sent to {i['email']} <<<<<")
+            print(f">>>> email sent to {i['email']} <<<<<")
+
+    print(f"This took {datetime.now() - start_time} to run")
+    logging.info(f"This took {datetime.now() - start_time} to run")
+
+if __name__ == '__main__':
+    main()
